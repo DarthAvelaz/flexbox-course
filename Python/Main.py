4da5ead90e200c9ba6290e1a77ae90e50f06d97b
DarthAvelaz <kornfan2291@yahoo.com>
2023-05-02 23:35:29 -0500

    Python course lessons

---
+++ Python/Main.py
@@ -0,0 +1,19 @@
+#sentence = 'The quick brown fox jumped'
+#sentence -> variable
+#'The quick brown fox jumped' -> string
+# = -> assignment
+
+#sentence = 'The quick brown fox jumped'
+#sentence_two = sentence.upper()
+
+
+#print(sentence)
+#print(sentence_two)
+#sentence = 'The quick brown fox jumped' .capitalize()
+#print(sentence)
+
+#sentence ='The quick brown fox jumped' .title()
+#print(sentence)
+
+sentence = "Some pie please!"
+sub_sentence = sentence[5:8]
