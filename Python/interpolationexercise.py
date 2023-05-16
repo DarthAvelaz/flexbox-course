a18164c075bd0fcbee11f9726c1023ffb529864b
DarthAvelaz <kornfan2291@yahoo.com>
2023-05-15 21:21:35 -0500

    interpolation added other modules exercises

--- Python/interpolationexercise.py
+++ Python/interpolationexercise.py
@@ -3,7 +3,15 @@ last_name = 'Velazquez'
 
 def function(first_name, last_name):
     greeting = f"""
-    Hello, (first_name, last_name)
+    'Hi,{first_name} {last_name}'
     """
+    return(greeting)
 
-    return (greeting)
+
+#name = 'Kristine'
+#age = '12'
+#product = 'Python elearning course'
+
+#greeting = "Hi {0}, you are listed as {1} years old and you have purchased: {2}...".format(name, age, product)
+
+#print(greeting)
