3a948659145d0d80d265ea35b7492897ba979b60
DarthAvelaz <kornfan2291@yahoo.com>
2023-05-24 20:29:50 -0500

    Checkpoint 3 corrections

--- Python/Course3CP.py
+++ Python/Course3CP.py
@@ -1,17 +1,17 @@
 #exercise 1#
 string = "hello, World"
 number_rand = 57
-list_seq = 1, 2, 3, 4, 5
+list_seq =[ 1, 2, 3, 4, 5,4,3,4532,432]
 controller_up = True
 
 #exercise 2#
-query = string.index('l')
+query = string[0:3]
 print(query)
 
 index_store = 2
 
 #exercise 3#
-query_two = list_seq.index(1)
+query_two = list_seq[0]
 print(query_two)
 
 #exercise 4#
@@ -19,8 +19,8 @@ new_number = number_rand + 10
 print(new_number)
 
 #exercise 5#
-#query_three = list_seq.index(6)
-#rint(query_three)
+query_three = list_seq[-1]
+print(query_three)
 
 #exercise 6#
 names = 'harry,alex,susie,jared,gail,conner'
@@ -39,5 +39,5 @@ number_count = f'Your number is {number_rand}'
 print(number_count)
 
 #exercise 9
-hello_world = "hello world"
-print(hello_world)
+post = "hello world"
+print(post)
