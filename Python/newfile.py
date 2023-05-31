1b831e086deb5d25dea77e313c899ddcab839bdc
DarthAvelaz <kornfan2291@yahoo.com>
2023-05-30 20:56:17 -0500

    Numbers & expressions lessons

---
+++ Python/newfile.py
@@ -0,0 +1,31 @@
+product_id = 123
+sale_price = 14.99
+tip_percentage = 1/5
+new_product = 150
+
+#print(sale_price + new_product)
+#print(product_id + new_product)
+
+
+print('Addition')
+print(100 + 42)
+
+print('Subtraction')
+print(100 - 42)
+
+print('Division')
+print(100 / 42)
+print(100 / 38)
+
+print('Floor Division')
+print(100 // 42)
+print(100 // 38)
+
+print('Multiplication')
+print(100 * 42)
+
+print('Modulus')
+print(100 % 42)
+
+print('Exponents')
+print(100 ** 42)
