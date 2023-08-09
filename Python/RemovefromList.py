5bcec6e2c6f0c35124fdaab98bc9d39a2ba1f86d
DarthAvelaz <kornfan2291@yahoo.com>
2023-08-08 21:31:31 -0500

    Removing first and last from a list

---
+++ Python/RemovefromList.py
@@ -0,0 +1,21 @@
+"""
+remove_first_and_last(list_to_clean)
+
+html = ['<h1>', 'Some content', '</h1>']
+
+remove_first_and_last(html)
+=> ['Some content']
+html_2 = ['<h1>', 'Some content', 'more', '</h1>']
+
+remove_first_and_last(html_2)
+=> ['Some content', 'more']
+one, *two, three = [1, 2, 3, 4, 5]
+"""
+def remove_first_and_last(list_to_clean):
+    _, *content, _ = list_to_clean
+    return content
+
+
+html = ['<h1', 'Some content', '</h1>']
+
+print(remove_first_and_last(html))
